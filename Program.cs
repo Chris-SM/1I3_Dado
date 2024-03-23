096c9233579a40b918c01bf271e2435e84e08950
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-22 22:36:09 -0300

    Finalizado Criador de dado e rolagem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+
+Console.Write("Digite a quantidade de lados o dado tem: ");
+int Dado = int.Parse(Console.ReadLine()!);
+Dado ++;
+int res = RandomNumberGenerator.GetInt32(1,Dado);
+Console.Write("O numero rolado foi... ");
+Thread.Sleep(1500);
+Console.Write(res);
